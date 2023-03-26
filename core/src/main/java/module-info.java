e48817e6848b7175febbb3363e331b411870609b
grimmJ04 <hoda@inf.u-szeged.hu>
2023-03-26 18:52:42 +0200

    Removed some deps; Added core module export on repositories;

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -5,6 +5,7 @@ module courses.core {
     requires spring.data.commons;
 
     exports hu.szte.inf.models;
+    exports hu.szte.inf.repositories;
     exports hu.szte.inf.utils;
     exports hu.szte.inf.utils.db;
 }
